e8eba15c13a341e4bc42a235c7a3d8d87a82f4a4
Lawrence Wu <lawrence@lawrencewu.me>
2016-05-03 18:20:16 -0400

    add icon, fix up debugging

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 var menubar = require('menubar')
-var debug = false;
+var debug = true;
+
+var mb = menubar({
+  width: 200,
+  height: 310,
+  resizable: false,
+})
 
 if (debug) {
-  var mb = menubar({
-    width: 400,
-    height: 600,
-    resizable: false
-  })
-
   mb.on('after-create-window', function() {
-    mb.window.openDevTools()
-  })
-} else {
-  var mb = menubar({
-    width: 200,
-    height: 310
+    mb.window.openDevTools({detach: true})
   })
 }
-
