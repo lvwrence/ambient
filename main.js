197782aee577e2a83dc544470e81aeeb4d6a66bd
Lawrence Wu <lawrence@lawrencewu.me>
2016-05-04 14:06:23 -0400

    add volume control

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ var debug = true;
 
 var mb = menubar({
   width: 200,
-  height: 362,
+  height: 370,
   resizable: false,
+  "always-on-top": true
 })
 
 if (debug) {
   mb.on('after-create-window', function() {
-    mb.window.openDevTools({detach: true})
+     mb.window.openDevTools({detach: true})
   })
 }
